7442d9caa8a608f95fc2eacf65908ae16d96e84f
yuna04 <joakimtj@hiof.no>
2024-08-30 18:18:37 +0200

    oppg 6 done

--- Oppgave_00/src/index.js
+++ Oppgave_00/src/index.js
@@ -44,31 +44,18 @@ btnList.addEventListener('click', () => {
 // Oppgave 5
 // Lag lytterene og funksjonene som gjør at du kan lage et et HTML element (gjerne fra en nedtrekkslist med id #select) 
 // og tekst fra inputboksen med id #text. Bruk knappen med id #create. Legg teksten til elementet med id #placeholder
-let dropDown = document.querySelector('#select'); // #select
-/* Stupid but I'll keep it here for future reference.
-let dropDownSelection = 'h2';
-dropDown.addEventListener('change', () => {
-    console.log(dropDown.value);
-    dropDownSelection = dropDown.value;
-}) 
-*/ 
-// let textString = '';
-let textField = document.querySelector('#text'); // #text
-/* Same naive thing going on here as earlier lol
-textField.addEventListener('input', (e) => {
-    if (e.data === null) {
-        let strlen = textString.length;
-        textString = textString.slice(0, strlen-1);
-    } else {
-        let content = e.data;
-        textString += content;
-        console.log(textString);
-    }
-})
-*/
+// Originally had solved this with eventlisteners for dropdown (change) and textfield (input).
+// Both would store the content in their own string variables.
+// Then I figured out that was really dumb lol
+const dropDown = document.querySelector('#select'); // #select
+const textField = document.querySelector('#text'); // #text
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
-let textCreate = document.querySelector('#create') // #create
+const textCreate = document.querySelector('#create') // #create
 textCreate.addEventListener('click', () => {
+    if (textField.value === '') {
+        alert(`Please input something..... :(`);
+        return;
+    }
     const placeholder = document.querySelector('#placeholder');
     const createdElement = document.createElement(dropDown.value);
     const elementContent = document.createTextNode(textField.value);
@@ -76,5 +63,17 @@ textCreate.addEventListener('click', () => {
     placeholder.appendChild(createdElement);
 })
 // Oppgave 6
+// Lag lytteren til knappen med id #remove-li og funksjonen som gjør 
+// at du ved hvert klikk sletter et og et li element fra ul med id #list
+const removeButton = document.querySelector('#remove-li');
+removeButton.addEventListener('click', () => {
+    // Could also simply select the list itself (#list) and then do listItemToRemove.lastChild.remove
+    // but when doing that, it requires two clicks to delete an item and I can't be bothered to debug it.
+    const listItemToRemove = document.querySelector('#list li');
+    if (listItemToRemove === null) {
+        console.log('li returned null.')
+        console.log('List likely empty.')
+    } else listItemToRemove.remove();
+})
 // Oppgave 7
 // Oppgave 8
