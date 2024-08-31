e2a6ee47d5566519dfcdf96dd87a5c41ab30f3f4
yuna04 <joakimtj@hiof.no>
2024-08-31 07:30:10 +0200

    Oppgave 01 completed.

--- Oppgave_01/src/start.js
+++ Oppgave_01/src/start.js
@@ -1,13 +1,22 @@
-import './styles.css';
-
 // Statisk tekst som brukes til å søke i
 const text =
   'Baby cliche unicorn brooklyn farm-to-table. Salvia semiotics hella literally paleo humblebrag bushwick letterpress messenger bag pork belly brooklyn authentic vexillologist. Gastropub sustainable banjo, shaman snackwave viral air plant ramps health goth. Edison bulb vegan microdosing, tote bag unicorn skateboard disrupt copper mug four loko sustainable whatever cloud bread slow-carb lumbersexual four dollar toast. Waistcoat lomo hammock vape shabby chic sartorial yr godard pok pok fashion axe organic migas. Quinoa yr vexillologist intelligentsia verylongwordthatislong neutra mixtape YOLO XOXO listicle letterpress farm-to-table beard.';
 
 const longestWord = () => {
-  // TODO: Gjør om text til array eks. ['Baby', 'cliche']
-  // TODO: Velg første ord så du har noe å sammenlikne med
-  // TODO: Gå igjennom alle ordene og oppdater hvis nytt ord er lengre
+  let array = text.split(/\W+/);
+  array.pop(); // Remove the trailing empty string ''
+  
+  let longestWord = array[0];
+  for (let i = 0; i < array.length; i++) 
+    if (longestWord.length < array[i].length)
+      longestWord = array[i];
+  
+  return longestWord;
 };
 
+const div = document.createElement('div');
+const content = document.createTextNode(`The longest word is: ${longestWord()}`);
+div.appendChild(content);
+const body = document.querySelector('body');
+body.appendChild(div);
 console.log(longestWord());
