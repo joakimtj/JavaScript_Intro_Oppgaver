f4bca9b6f519ab1a63cadbc5b1e0a43957a36271
yuna04 <joakimtj@hiof.no>
2024-09-02 18:59:03 +0200

    i never committed dis????

--- Oppgave_02/src/ingen_hjelp.js
+++ Oppgave_02/src/ingen_hjelp.js
@@ -2,6 +2,7 @@ import './styles.css';
 import { users } from './util';
 
 // TODO: Hent HTML #id med getElementById
+console.log("hello");
 // TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
 // TODO: Lag en funksjon som håndterer søket og oppdaterer grensesnittet med resultatet fra søket
 // TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
